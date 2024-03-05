d22859b09160b48905565e5dfc789dbd02e8c207
abdul-zacky <158559854+abdul-zacky@users.noreply.github.com>
2024-03-05 14:13:31 +0700

    Update OrderGenerator.java

--- assignment1/src/main/java/assignments/assignment1/OrderGenerator.java
+++ assignment1/src/main/java/assignments/assignment1/OrderGenerator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TP1 {
+public class OrderGenerator {
 
     public static void showMenu() {
         System.out.println(">>=======================================<<");
